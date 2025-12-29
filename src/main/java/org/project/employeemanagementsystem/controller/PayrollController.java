b4e69e2115f6e7699e658783aa5f0578ba369ce5
HarryGamer6 <g270804@gmail.com>
2025-12-29 20:52:47 +0200

    yea

---
+++ src/main/java/org/project/employeemanagementsystem/controller/PayrollController.java
@@ -0,0 +1,8 @@
+package org.project.employeemanagementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PayrollController extends BaseController {
+
+}
