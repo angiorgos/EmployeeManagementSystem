94396f86b70e1780fec58a16178639ef3defe70b
Michael George Mallas <michael01mallas@gmail.com>
2026-01-02 00:58:02 +0200

    updating the ui and the controller of departments

--- src/main/java/org/project/employeemanagementsystem/controller/DepartmentController.java
+++ src/main/java/org/project/employeemanagementsystem/controller/DepartmentController.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
-
+//test
 @Controller
 public class DepartmentController implements Initializable {
 
