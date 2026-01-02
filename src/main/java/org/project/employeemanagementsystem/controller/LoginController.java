937ccec14352a98604e8e7886e60a95d6865cf7b
angiorgos12@gmail.com <angiorgos12@gmail.com>
2026-01-02 18:55:16 +0200

    almost finished

--- src/main/java/org/project/employeemanagementsystem/controller/LoginController.java
+++ src/main/java/org/project/employeemanagementsystem/controller/LoginController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import org.project.employeemanagementsystem.model.User;
 import org.project.employeemanagementsystem.service.AuthService;
+import org.project.employeemanagementsystem.service.SystemLogService;
 import org.project.employeemanagementsystem.util.Navigator;
 import org.project.employeemanagementsystem.util.UserSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class LoginController {
 
-    @Autowired
-    private AuthService authService;
+    @Autowired private AuthService authService;
+    @Autowired private Navigator navigator;
+    @Autowired private UserSession userSession;
 
-    @Autowired
-    private Navigator navigator;
-
-    @Autowired
-    private UserSession userSession;
+    // ΑΥΤΟ ΔΙΟΡΘΩΝΕΙ ΤΟ ΣΦΑΛΜΑ ΠΡΟΣΒΑΣΗΣ
+    @Autowired private org.project.employeemanagementsystem.service.SystemLogService logService;
 
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
@@ -36,8 +35,14 @@ public class LoginController {
 
         if (user != null) {
             userSession.login(user);
+
+            // ΤΩΡΑ ΔΟΥΛΕΥΕΙ: Καλούμε το τοπικό logService
+            logService.log("User Login Success");
+
             navigator.goToDashboard();
         } else {
+            // Καταγραφή αποτυχίας (θα εμφανιστεί ως "System" στα logs)
+            logService.log("Failed login attempt for username: " + username);
             errorLabel.setText("Wrong credentials!");
         }
     }
