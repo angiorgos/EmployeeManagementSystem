ea46b84f5d0b0ad353687f4781103e17b81e6769
angiorgos12@gmail.com <angiorgos12@gmail.com>
2026-01-16 22:03:44 +0200

    comments

--- src/main/java/org/project/employeemanagementsystem/EmployeeManagementSystemApplication.java
+++ src/main/java/org/project/employeemanagementsystem/EmployeeManagementSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+//boot κλάση
 public class EmployeeManagementSystemApplication {
 
     public static void main(String[] args) {
